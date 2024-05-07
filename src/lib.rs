967dc52e07cf705c02de06e243241035a5fb81d9
liyan-ah <liyan-ah@formail.com>
2024-05-07 19:54:29 +0800

    Init a trait.

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+pub trait Check {
+    fn check(&self, arg: i32) -> bool;
+}
+
+struct TypeCheck {
+    level: i32,
+}
+
+impl Check for TypeCheck {
+    fn check(&self, arg: i32) -> bool {
+        self.level > arg
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::sync::Arc;
+
+    fn as_check(check: Arc<Box<dyn Check>>) {
+        assert!(check.check(11));
+    }
+
+    #[test]
+    fn type_check() {
+        let check:Arc<Box<dyn Check>> = Arc::new(Box::new(TypeCheck { level: 12 }));
+
+        assert!(check.check(11));
+        as_check(check);
+    }
+}
